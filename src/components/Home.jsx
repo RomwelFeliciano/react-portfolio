e568ddcacb92ff035662cb724cf3ec907b94f502
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-07-11 16:16:41 +0800

    Responsive Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <>
       <div className="w-full min-h-screen items-center grid grid-rows-1 grid-cols-1 lg:grid-cols-[10%_40%_50%] px-10">
-        <div className="w-[200px] flex flex-col gap-10 text-3xl justify-center items-center">
+        <div className="w-[200px] hidden lg:flex md:flex-col gap-10 text-3xl justify-center items-center">
           <a href="#FB" target="_blank">
             <BsFacebook className="cursor-pointer hover:text-[#4267B2] hover:scale-125 duration-300" />
           </a>
@@ -27,11 +27,11 @@ const Home = () => {
         <div className="lg:mt-0 mt-24 relative mx-auto bg-gradient-to-b from-[#ffffff] rounded-full md:w-80 md:h-80 w-72 h-72 overflow-hidden">
           <img className="relative object-cover" src="./profile.png" alt="" />
         </div>
-        <div className="xl:w-100 xl:w-3/4 md:text-left text-center">
+        <div className="xl:w-100 xl:w-3/4 lg:text-left text-center">
           <h2 className="md:text-5xl text-4xl font-bold text-main lg:pt-0 pt-5">
             {profile.name}
           </h2>
-          <h3 className="typed flex md:justify-start justify-center text-2xl gap-2 font-normal text-second md:text-4xl pt-2">
+          <h3 className="typed flex lg:justify-start justify-center text-2xl gap-2 font-normal text-second md:text-4xl pt-2">
             I'm a
             <TypeAnimation
               sequence={[
@@ -48,6 +48,18 @@ const Home = () => {
             />
           </h3>
           <p className="font-normal leading-7 pt-2">{profile.description}</p>
+        </div>
+        <div className="lg:hidden flex gap-10 text-3xl justify-center items-center py-10 lg:py-0">
+          <a href="#FB" target="_blank">
+            <BsFacebook className="cursor-pointer hover:text-[#4267B2] hover:scale-125 duration-300" />
+          </a>
+          <a href="#LI" target="_blank">
+            <BsLinkedin className="cursor-pointer hover:text-[#0077B5] hover:scale-125 duration-300" />
+          </a>
+
+          <a href="#GH" target="_blank">
+            <BsGithub className="cursor-pointer hover:text-[#1D1D1D] hover:scale-125 duration-300" />
+          </a>
         </div>
       </div>
     </>
