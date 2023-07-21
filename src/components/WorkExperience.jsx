d5ee323dc5532ad368a8eac410847554c23e719b
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-07-21 19:59:05 +0800

    Data Objects of Experiences

---
+++ src/components/WorkExperience.jsx
@@ -0,0 +1,19 @@
+export const work = [
+  {
+    level: "OJT and Internship",
+    company: "Kloudtech Corp.",
+    role: "Front-End Developer and UI/UX Designer",
+    year: "March 2023 - Present",
+    image:
+      "https://scontent.fmnl17-1.fna.fbcdn.net/v/t39.30808-6/358541226_247849084766856_2409439841455213300_n.jpg?_nc_cat=101&cb=99be929b-3346023f&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGg-SvuLY_nuuAEYNwuGH67K-2-_MK8pxgr7b78wrynGDnm_3pnYOE31kI_gmpd16slWyuT7k1jbQsbsQdrti63&_nc_ohc=A2IUVdjNYTIAX_YmqPj&_nc_ht=scontent.fmnl17-1.fna&oh=00_AfAYTlC2Sld-_uGsku5L1NchbMMZmFeQAwfWHGPArdDXkg&oe=64BFC923",
+    tasks: ["Task 1", "Task 2", "Task 3"],
+  },
+  {
+    level: "Work Immersion",
+    company: "LGU Orani",
+    role: "IT Support",
+    year: "December 2018",
+    image: "https://live.staticflickr.com/6005/5947680623_3e8949486d_z.jpg",
+    tasks: ["Task 1", "Task 2", "Task 3"],
+  },
+];
