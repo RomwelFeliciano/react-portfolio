fa8a0cac412211ba3de374557bc811ab7953a2bf
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-07-22 20:30:44 +0800

    Added Link Logo to Slide in Home

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
     <div className="shadow-md w-full fixed top-0 left-0 z-10">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
         <div className="font-bold text-2xl cursor-pointer flex justify-between items-center">
-          <img className="w-14" src="logo.png" alt="" />
+          <a href="#Home"><img className="w-14" src="logo.png" alt="" /></a>
 
           <div
             className="text-3xl right-8 top-6 cursor-pointer md:hidden"
