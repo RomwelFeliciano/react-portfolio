302b507de966a53dbced39434575092a8c94d90a
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-07-26 21:24:20 +0800

    Removed Shadow in ul

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -30,7 +30,7 @@ const Nav = () => {
           </div>
         </div>
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white shadow-lg md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
             open ? "top-16 " : "top-[-490px]"
           }`}
         >
