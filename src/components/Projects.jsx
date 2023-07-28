32e22fb8acc75feac3e736ee06cbbac04fd68f74
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-07-28 15:18:39 +0800

    Added Framer Motion for Filtering

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import { projects } from "./Data";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Projects = () => {
-  const [showProject, setShowProject] = useState("all");
+  const [showProject, setShowProject] = useState("");
   const [project, setProject] = useState(projects);
   const [filtered, setFiltered] = useState(projects);
 
@@ -31,7 +32,7 @@ const Projects = () => {
   return (
     <section
       id="Projects"
-      className="container flex flex-col justify-center items-center p-2 md:p-10 min-h-screen"
+      className="container flex flex-col justify-center items-center p-2 md:p-10"
     >
       <div className="flex flex-col justify-center items-center gap-2">
         <h2 className="text-xl text-second">Projects I Have</h2>
@@ -67,35 +68,51 @@ const Projects = () => {
         </div>
       </div>
       {/* Container of Cards */}
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-3 rounded-sm">
-        {/* Cards for each data 
+      <motion.div
+        layout
+        className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-3 rounded-sm"
+      >
+        <AnimatePresence>
+          {/* Cards for each data 
           Mapping Starts Here*/}
-        {filtered &&
-          filtered.map((proj) => (
-            <div className="bg-white shadow-sm rounded-sm" key={proj.title}>
-              <div className="p-4">
-                <img src={proj.image} alt="" className="object-cover rounded" />
-              </div>
-              <div className="px-5 pb-3">
-                <h1 className="text-2xl text-main">{proj.title}</h1>
-                <p className="text-md">{proj.description}</p>
-              </div>
-              <div className="px-5 pb-4">
-                <div className="flex flex-wrap items-center gap-3">
-                  {proj.tools &&
-                    proj.tools.map((tags) => (
-                      <h2
-                        className="bg-main text-sm rounded-lg px-2 py-1"
-                        key={tags}
-                      >
-                        #{tags}
-                      </h2>
-                    ))}
+          {filtered &&
+            filtered.map((proj) => (
+              <motion.div
+                layout
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+                className="bg-white shadow-sm rounded-sm"
+                key={proj.title}
+              >
+                <div className="p-4">
+                  <img
+                    src={proj.image}
+                    alt=""
+                    className="object-cover rounded"
+                  />
                 </div>
-              </div>
-            </div>
-          ))}
-      </div>
+                <div className="px-5 pb-3">
+                  <h1 className="text-2xl text-main">{proj.title}</h1>
+                  <p className="text-md">{proj.description}</p>
+                </div>
+                <div className="px-5 pb-4">
+                  <div className="flex flex-wrap items-center gap-3">
+                    {proj.tools &&
+                      proj.tools.map((tags) => (
+                        <h2
+                          className="bg-main text-sm rounded-lg px-2 py-1"
+                          key={tags}
+                        >
+                          #{tags}
+                        </h2>
+                      ))}
+                  </div>
+                </div>
+              </motion.div>
+            ))}
+        </AnimatePresence>
+      </motion.div>
     </section>
   );
 };
